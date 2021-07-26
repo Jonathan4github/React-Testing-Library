f19e8067f8155ee279df11bae17efba1f6c13808
Jonathan Williams <31247452+Jonathan4github@users.noreply.github.com>
2021-07-26 07:26:16 +0100

    write test for the footer component

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -2,14 +2,12 @@ import React from 'react'
 import "./TodoFooter.css"
 import { Link } from "react-router-dom"
 
-function TodoFooter({
-    numberOfIncompleteTasks
-}) {
-    return (
-        <div className="todo-footer">
-            <p>{numberOfIncompleteTasks} {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left</p>
-            <Link to="/followers">Followers</Link>
-        </div>
+const TodoFooter = ({ numberOfIncompleteTasks}) => {
+  return (
+    <div className="todo-footer">
+      <p>{numberOfIncompleteTasks} {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left</p>
+      <Link to="/followers">Followers</Link>
+     </div>
     )
 }
 
